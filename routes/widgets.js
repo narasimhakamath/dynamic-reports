9659dacacdd3d5c5faf12865c87ad3b9f2d1b77a
Narasimha Kamath <narasimha@datanimbus.com>
2025-05-12 09:37:27 +0530

    - Added the labels for the widgets.

--- routes/widgets.js
+++ routes/widgets.js
@@ -104,6 +104,8 @@ router.get('/', async (req, res) => {
       name: widget?.widget?.name,
       description: widget?.widget?.description,
       type: widget?.widget?.type,
+      xAxisLabel: widget?.widget?.xAxisLabel,
+      yAxisLabel: widget?.widget?.yAxisLabel,
       options: widget?.widget?.options,
     }));
 
